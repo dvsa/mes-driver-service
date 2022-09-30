9ee7476030cb243751e8d660f17bc28d4f284a0e
Georgia Margetts <48907036+georgiamarg1@users.noreply.github.com>
2022-09-30 13:36:17 +0100

    MES-8248/created signature endpoint & handler (#2)
    * created signature endpoint & handler
    
    * created Sig interface, created tests for Signature EP

---
+++ src/common/application/driver/FindDriverSignature.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { error } from '@dvsa/mes-microservice-common/application/utils/logger';
+import { getDriverAPIKey, getDriverBaseEndpoint } from '../../framework/DriverEndpoint';
+import { HttpStatus } from '../api/HttpStatus';
+import { DriverSignature } from '../../domain/driver-signature.interface';
+
+const axiosInstance = axios.create();
+
+export async function findDriverSignature(
+  drivingLicenceNumber: string,
+  token: string,
+): Promise<DriverSignature | null> {
+  try {
+    const URL: string = `${getDriverBaseEndpoint()}/image/signature`;
+
+    const response = await axiosInstance.post(
+      URL,
+      JSON.stringify({ drivingLicenceNumber }),
+      {
+        headers: {
+          Authorization: token,
+          'x-api-key': getDriverAPIKey(),
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    if (response.status === HttpStatus.OK) {
+      return response.data;
+    }
+    return null;
+  } catch (err: unknown) {
+    error(err as string);
+    throw err;
+  }
+}
